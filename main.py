20e02afbc0d4416da00ae2da60c99f9a7280a6f0
Gavin Yip <gavin.yip88@gmail.com>
2024-03-08 18:03:27 +0000

    runninglocallcy

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ from flask import Flask, render_template, request
 import qrcode
 from io import BytesIO
 from base64 import b64encode
-import os
+
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-@app.route('/', methods=['POST'])
-def generateQR():
-    memory = BytesIO()
-    data = request.form.get('url')
-    img = qrcode.make(data)
-    img.save(memory)
-    memory.seek(0)
-    base64_img = "data:image/png;base64," +  b64encode(memory.getvalue()).decode('ascii')
-
-    return render_template('index.html',  data=base64_img)
+    if request.method == 'POST':
+        memory = BytesIO()
+        data = request.form.get('url')
+        img = qrcode.make(data)
+        img.save(memory)
+        memory.seek(0)
+        base64_img = "data:image/png;base64," +  b64encode(memory.getvalue()).decode('ascii')
+        return render_template('index.html',  data=base64_img)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
