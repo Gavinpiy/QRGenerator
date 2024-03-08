74ee9149eabd91c0e579c0f41df041c16272786b
Gavin Yip <gavin.yip88@gmail.com>
2024-03-08 17:22:46 +0000

    updated gunicorn

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, request
 import qrcode
 from io import BytesIO
 from base64 import b64encode
+import os
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 @app.route('/')
 def home():
     return render_template('index.html')
